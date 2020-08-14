0f68e7ad5e2a5501e4d36ae364f58014339e8c94
ibfahd <ibfahd@gmail.com>
2020-08-14 17:01:29 +0100

    Done

--- Sorting Comparator.py
+++ Sorting Comparator.py
@@ -2,10 +2,25 @@
 from functools import cmp_to_key
 class Player:
     def __init__(self, name, score):
-        
+        self.name = name
+        self.score = score
+
     def __repr__(self):
-        
+        return "{0}, {1}".format(name, score)
+    
+    @staticmethod
     def comparator(a, b):
+        if a.score > b.score:
+            return -1
+        elif a.score < b.score:
+            return 1
+        else:
+            if a.name > b.name:
+                return 1
+            elif a.name < b.name:
+                return -1
+            else:
+                return 0
 
 n = int(input())
 data = []
